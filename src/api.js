c31a2f2aede16d77f86624e1756b200b375623a4
robbe-tm <95742230+robbe-tm@users.noreply.github.com>
2022-10-20 09:34:18 +0200

    InitialCreate

---
+++ src/api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const serverless = require('serverless-http');
+
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use('/.netlify/functions/api', router);
+
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+
+module.exports.handler = serverless(app);
---
+++ src/routes.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const Campus = require('./models/campus');
+
+router.get('/', (req, res) => {
+    console.log('/ route called');
+    res.send(
+        '<h1>Welcome to my API, these are the available routes:</h1>'
+
+        +
+        '<h2>/</h2>' +
+        'Where you are right now'
+
+        +
+        '<hr/>'
+
+        +
+        '<h2>/campus</h2>' +
+        'Returns all campuses in the database using .find()'
+
+        +
+        '<hr/>'
+    );
+});
+
+router.get('/campus', async(req, res) => {
+    console.log('/campus route called');
+    try {
+        res.json(await Campus.find());
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+router.get('/campus/:id', async(req, res) => {
+    console.log('/campus/:id route called');
+    try {
+        res.json(await Campus.findById(req.params.id));
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+router.post('/campus/create', async(req, res) => {
+    console.log('/campus/create route called');
+    try {
+        res.json(await Campus.create(req.body));
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+router.put('/campus/update/:id', async(req, res) => {
+    console.log('/campus/update/:id route called');
+    try {
+        res.send(await Campus.findByIdAndUpdate(req.params.id, { $set: req.body }));
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+module.exports = router;
